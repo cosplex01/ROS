bbd93442f830e2c076defae4a20b3ee3c7884960
천리마민방위 잎사귀 <48374148+cosplex01@users.noreply.github.com>
2020-08-12 15:57:46 +0900

    오브젝트가 경로상에 있는 것은 파악하는 부분까지 입니다
    경로상에 오브젝트가 있는 것은 시뮬레이터가 전달해 줍니다

--- checkridar/scripts/5_purepursuit_advanced.py
+++ checkridar/scripts/5_purepursuit_advanced.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import rospy
 import rospkg
@@ -14,15 +14,14 @@ from nav_msgs.msg import Odometry,Path
 import tf
 from tf.transformations import euler_from_quaternion,quaternion_from_euler
 
+
 class pure_pursuit :
     def __init__(self):
         rospy.init_node('pure_pursuit', anonymous=True)
         rospy.Subscriber("path", Path, self.path_callback)
         rospy.Subscriber("odom", Odometry, self.odom_callback)
-        # 시뮬레이터에서 차량의 속도와 방향을 가진 토픽을 얻기
-        rospy.Subscriber("sensor/core", VescStateStamped, self.vesc_callback)
-        # 사용자가 시뮬레이터에 지정하는 토픽을 생성하자
-        rospy.Subscriber("user/speed", Float64, self.user_callback)
+        rospy.Subscriber("/sensors/core", VescStateStamped, self.status_callback)
+        rospy.Subscriber("/target_vel", Float64, self.vel_callback)
         # rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.amcl_callback)
         self.motor_pub = rospy.Publisher('commands/motor/speed',Float64, queue_size=1)
         self.servo_pub = rospy.Publisher('commands/servo/position',Float64, queue_size=1)
@@ -30,60 +29,57 @@ class pure_pursuit :
         self.servo_msg=Float64()
         self.is_path=False
         self.is_odom=False
-        self.is_amcl=False
-        self.is_vesc=False
         self.is_speed=False
+        self.is_target_vel=False
         self.forward_point=Point()
         self.current_postion=Point()
         self.is_look_forward_point=False
         self.vehicle_length=1
-        # 차량의 초기값
-        self.user_speed=50000
-        # localpath의 리딩 초기값
-        self.lfd=2.5
+        self.lfd=3
+        self.min_lfd=3
+        
         self.steering=0
 
+        self.rpm_gain=4614
         self.steering_angle_to_servo_gain =-1.2135
         self.steering_angle_to_servo_offset=0.5304   
         rate = rospy.Rate(30) # 30hz
         while not rospy.is_shutdown():
 
-            if self.is_path ==True and (self.is_odom==True or self.is_amcl==True or self.is_vesc==True) :
+            if self.is_path ==True and self.is_odom==True and self.is_speed==True :
                 
                 vehicle_position=self.current_postion
                 rotated_point=Point()
                 self.is_look_forward_point= False
-
-                # 시뮬레이션에서 보내주는 차량제어 토픽이 맞는가
-                if self.is_vesc == True:
-                    # 차량속도를 지정한다
-                    vehicle_speed = self.vehicle_speed/4616*3.6
-                    if vehicle_speed < 5:
-                        self.lfd = 2.5
-                    elif vehicle_speed >= 5 and vehicle_speed < 20:
-                        self.lfd = 0.5 * vehicle_speed
-                    else:
-                        self.lfd = 10.0
-
+    
                 for num,i in enumerate(self.path.poses) :
                     path_point=i.pose.position
                     dx= path_point.x - vehicle_position.x
                     dy= path_point.y - vehicle_position.y
                     rotated_point.x=cos(self.vehicle_yaw)*dx +sin(self.vehicle_yaw)*dy
                     rotated_point.y=sin(self.vehicle_yaw)*dx - cos(self.vehicle_yaw)*dy
-            
-                    dis=sqrt(pow(rotated_point.x,2)+pow(rotated_point.y,2))
-                    if dis >= self.lfd :
-                        self.forward_point=path_point
-                        self.is_look_forward_point=True
-                        break
+                    
+                    if rotated_point.x>0 :
+                        dis=sqrt(pow(rotated_point.x,2)+pow(rotated_point.y,2))
+                        if dis>= self.lfd :
+                            self.lfd=self.speed/1.8
+                            if self.lfd< self.min_lfd :
+                                self.lfd=self.min_lfd
+                            self.forward_point=path_point
+                            self.is_look_forward_point=True
+                        
+                            break
         
                 theta=-atan2(rotated_point.y,rotated_point.x)
                 if self.is_look_forward_point :
                     self.steering=atan2((2*self.vehicle_length*sin(theta)),self.lfd) #rad
-                    print(self.steering*180/pi) #degree
-                    self.motor_msg.data= self.user_speed
-
+                    
+                    if self.is_target_vel==True :
+                        self.motor_msg.data=self.target_vel*self.rpm_gain/3.6
+                    else :
+                        self.motor_msg.data=60000
+                    
+                    print(self.steering*180/pi,self.motor_msg.data/self.rpm_gain*3.6) #degree
                 else : 
                     self.steering=0
                     print("no found forward point")
@@ -94,6 +90,7 @@ class pure_pursuit :
                 
                 self.servo_pub.publish(self.servo_msg)
                 self.motor_pub.publish(self.motor_msg)
+
             rate.sleep()
 
     def path_callback(self,msg):
@@ -107,20 +104,15 @@ class pure_pursuit :
         self.current_postion.x=msg.pose.pose.position.x
         self.current_postion.y=msg.pose.pose.position.y
 
-    def amcl_callback(self,msg):
-        self.is_amcl=True
-        amcl_quaternion=(msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z,msg.pose.pose.orientation.w)
-        _,_,self.vehicle_yaw=euler_from_quaternion(amcl_quaternion)
-        self.current_postion.x=msg.pose.pose.position.x
-        self.current_postion.y=msg.pose.pose.position.y
-    
-    def vesc_callback(self,msg):
-        self.is_vesc=True
-        self.vehicle_speed =msg.state.speed
-    # 사용자가 차량 속도지정 토픽 데이터 전달
-    def user_callback(self,msg):
-        self.user_speed=msg.data
+    def status_callback(self,msg):
+        self.is_speed=True
+        rpm=msg.state.speed
+        self.speed=rpm/self.rpm_gain
+        # print(self.speed)
 
+    def vel_callback(self,msg):
+        self.is_target_vel=True
+        self.target_vel=msg.data
 
 if __name__ == '__main__':
     try:
--- checkridar/scripts/path_save.py
+++ checkridar/scripts/path_save.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 #-*- coding: utf-8 -*-
+#!/usr/bin/env python
 
 import rospy
 import rospkg
@@ -14,7 +14,7 @@ from nav_msgs.msg import Odometry,Path
 
 import tf
 from tf.transformations import euler_from_quaternion,quaternion_from_euler
-
+#경로파일을 읽어들인 다음 경로데이터를 ROS에 전달합니다
 class make_path :
     def __init__(self):        
         rospy.init_node('make_path',anonymous=True)
@@ -22,43 +22,77 @@ class make_path :
         #rospy.Subscriber('/odom', Odometry, self.odom_callback)
         #path data send to subscriber
         self.path_pub = rospy.Publisher('/path',Path, queue_size=1)
+        self.path_local = rospy.Publisher('/local_path',Path, queue_size=1)
         self.is_odom = False
         self.path_msg=Path()
+        #로컬 패스를 담을 공간을 지정하여 초기화
+        self.local_msg=Path()
         #self.path_msg.header.frame_id='/odom'
         #gps conncection to map loading is /map
         self.path_msg.header.frame_id='/map'
-        self.prev_x=0
-        self.prev_y=0
+        self.local_msg.header.frame_id='/map'       
+        self.prev_x=0.0
+        self.prev_y=0.0
+        
         #read on path at saveFile
         rospack=rospkg.RosPack()
         #save to path odometry in File that find pakageFile
         pkg_path=rospack.get_path('checkridar')
-        #make file on created Folder
+        #load and replay on path
         full_path=pkg_path+'/path'+'/path.txt'
         self.f=open(full_path,'r')
+
         #if path loading
         lines=self.f.readlines()
+        #로컬 패스에 최대치로 표시(읽어들일)구간 간격을 지정한다
+        self.local_index = 3
+        #읽어들인 패스의 데이터 인덱스범위를 지정한다(우선은 10000으로)
+        self.temp_index=10000
+            
         for line in lines:
             tmp=line.split()
             read_pose=PoseStamped()
             read_pose.pose.position.x = float(tmp[0])
             read_pose.pose.position.y = float(tmp[1])
-            read_pose.pose.orientation.w = 1
+            read_pose.pose.orientation.w = 1            
             self.path_msg.poses.append(read_pose)
-        #load and replay on path
-        rate = rospy.Rate(20)
-
-        #if path loading
-        self.f.close()
-
-        while not rospy.is_shutdown():    
-            #don`t close __init__        
-            #rospy.spin()
-            #close __init__ and load path
             self.path_pub.publish(self.path_msg)
-            rate.sleep()
-        #if path making 
-        #self.f.close()
+        self.f.close()
+        rate = rospy.Rate(20)        
+        #읽어들인 경로의 인덱스를 계산하기 위한 변수지정
+        linecount=len(self.path_msg.poses)
+        print(linecount)
+
+        #자동차 본인의 일부 구간경로를 읽어들인다
+        while not rospy.is_shutdown():    
+            #read on path at saveFile
+            rospack=rospkg.RosPack()
+            #make file on created Folder
+            full_path=pkg_path+'/path'+'/path.txt'
+            self.f2=open(full_path,'r')
+            #if path loading
+            lines=self.f2.readlines()
+            #읽어들인 패스의 데이터 인덱스범위를 지정한다(우선은 10000으로)
+            self.temp_index=10000
+
+            for line in lines:
+                tmp=line.split()
+                tmp2=tmp
+                read_pose1=PoseStamped()
+                read_pose1.pose.position.x = float(tmp[0])
+                read_pose1.pose.position.y = float(tmp[1])
+                read_pose1.pose.orientation.w = 1            
+                
+                nowline = sqrt((self.prev_x-float(tmp[0]))**2 + (self.prev_y-float(tmp[1]))**2)
+                if nowline<=self.temp_index:
+                    self.temp_index = nowline
+                    tmp2[0] = tmp[0]
+                    tmp2[1] = tmp[1]
+                self.local_msg.poses.append(read_pose1)
+                self.path_local.publish(self.local_msg)
+            #if path loading
+        self.f2.close()
+        rate.sleep()
     
 if __name__ == '__main__' :
     try:
